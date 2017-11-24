fbbd460939a57f059ca6a6a521a12e3529f24f3b
ykoh <kenshin579@gmail.com>
2017-11-24 16:03:37 +0900

    finish studying up to 5.1.2

---
+++ ch05/frank-mouse-pos-logger/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import {Component} from '@angular/core';
+import {MySpecialLoggerService} from "./my-special-logger.service";
+import {LogLevel} from "./log-level.enum";
+
+@Component({
+  selector: 'mpl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mpl works!';
+  logger: MySpecialLoggerService; //todo: 이거의 의미는?
+
+  constructor() {
+    this.logger = new MySpecialLoggerService(LogLevel.INFO);
+    this.testLoggerLevel();
+  }
+
+  private testLoggerLevel() {
+    console.log("============= Default(Info) Log Level ============");
+    this.logger.debug("test logging ... in debug");
+    this.logger.info("test logging ... in info");
+    this.logger.warn("test logging ... in warn");
+    this.logger.error("test logging ... in error");
+
+    this.logger.logLevel = LogLevel.DEBUG;
+    console.log("============= Debug Log Level ============");
+    this.logger.debug("test logging ... in debug");
+    this.logger.info("test logging ... in info");
+    this.logger.warn("test logging ... in warn");
+    this.logger.error("test logging ... in error");
+
+    this.logger.logLevel = LogLevel.WARN;
+    console.log("============= Warn Log Level ============");
+    this.logger.debug("test logging ... in debug");
+    this.logger.info("test logging ... in info");
+    this.logger.warn("test logging ... in warn");
+    this.logger.error("test logging ... in error");
+
+    this.logger.logLevel = LogLevel.ERROR;
+    console.log("============= Error Log Level ============");
+    this.logger.debug("test logging ... in debug");
+    this.logger.info("test logging ... in info");
+    this.logger.warn("test logging ... in warn");
+    this.logger.error("test logging ... in error");
+  }
+}
---
+++ ch05/frank-mouse-pos-logger/src/app/my-special-logger.service.ts
@@ -0,0 +1,60 @@
+import {Injectable} from '@angular/core';
+import {LogLevel} from "./log-level.enum";
+import * as format from 'date-fns/format';
+
+@Injectable()
+export class MySpecialLoggerService {
+  logLevel: LogLevel; //현재 서비스에 실정한 로그 레벨
+  logs: string[] = []; //속성은 과거 로그를 보관
+  private readonly MAX_HISTORY_CNT: number = 100; //보관할 로그의 최대수
+  private readonly TIME_FORMATTER: string = "YYYY-MM-DD HH:mm:ss.SSS"; //로그를 출력할때 함께 출력할 시간의 포멧 정보
+
+  constructor(logLevel: LogLevel) {
+    this.MAX_HISTORY_CNT = 100;
+    this.logLevel = logLevel;
+    this.logs = [];
+
+  }
+
+  debug(msg: string) {
+    this.log(LogLevel.DEBUG, msg);
+  }
+
+  info(msg: string) {
+    this.log(LogLevel.INFO, msg);
+  }
+
+  warn(msg: string) {
+    this.log(LogLevel.WARN, msg);
+  }
+
+  error(msg: string) {
+    this.log(LogLevel.ERROR, msg);
+  }
+
+  log(logLevel: LogLevel, msg: string) {
+    const logMsg = this.getFormattedLogMsg(this.logLevel, msg);
+    if (this.isProperLogLevel(logLevel)) {
+      console.log(logMsg);
+      this.keepLogHistory(logMsg);
+    }
+  }
+
+  private keepLogHistory(log: string) {
+    if (this.logs.length === this.MAX_HISTORY_CNT) {
+      this.logs.shift();
+    }
+    this.logs.push(log);
+  }
+
+  private getFormattedLogMsg(logLevel: LogLevel, msg: string) {
+    const curTimestamp = format(new Date(), this.TIME_FORMATTER);
+    return `[${LogLevel[logLevel]}] ${curTimestamp} - ${msg}`;
+  }
+
+  private isProperLogLevel(logLevel: LogLevel): boolean {
+    if (this.logLevel === LogLevel.DEBUG) return true;
+    return logLevel >= this.logLevel;
+  }
+
+}
