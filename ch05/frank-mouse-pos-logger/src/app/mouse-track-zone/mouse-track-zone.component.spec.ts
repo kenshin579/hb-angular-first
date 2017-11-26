e2f6ac256faff151c2301ea02392e0853c83e4fa
ykoh <kenshin579@gmail.com>
2017-11-26 10:39:21 +0900

    finish studying up to chap5 - unit test 작성

--- ch05/frank-mouse-pos-logger/src/app/mouse-track-zone/mouse-track-zone.component.spec.ts
+++ ch05/frank-mouse-pos-logger/src/app/mouse-track-zone/mouse-track-zone.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MouseTrackZoneComponent } from './mouse-track-zone.component';
+import {MySpecialLoggerService} from "../my-special-logger.service";
+import {AnotherLoggerService} from "../another-logger.service";
+import {LOG_LEVEL_TOKEN} from "../app.tokens";
+import {LogLevel} from "../log-level.enum";
+import {By} from "@angular/platform-browser";
 
 describe('MouseTrackZoneComponent', () => {
   let component: MouseTrackZoneComponent;
@@ -8,7 +13,8 @@ describe('MouseTrackZoneComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MouseTrackZoneComponent ]
+      declarations: [ MouseTrackZoneComponent ],
+      providers: [MySpecialLoggerService, AnotherLoggerService, { provide: LOG_LEVEL_TOKEN, useValue: LogLevel.INFO }]
     })
     .compileComponents();
   }));
@@ -21,5 +27,17 @@ describe('MouseTrackZoneComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('마우스 클릭할 때마다 로그가 적재되어야 한다.', () => {
+    expect(component).toBeTruthy();
+
+    const trackZone = fixture.debugElement.query(By.css('.track-zone'));
+    trackZone.triggerEventHandler("click", { clientX: 1, clientY: 1 });
+    expect((<MySpecialLoggerService>component.logger).logs.length).toEqual(1);
+
+    trackZone.triggerEventHandler("click", { clientX: 100, clientY: 10 });
+    trackZone.triggerEventHandler("click", { clientX: 200, clientY: 150 });
+    expect((<MySpecialLoggerService>component.logger).logs.length).toEqual(3);
   });
 });
