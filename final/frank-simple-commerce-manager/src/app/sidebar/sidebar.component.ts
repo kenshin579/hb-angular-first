c8b4376d166a52f0f01c44ad01d3884b52cc8300
ykoh <kenshin579@gmail.com>
2017-12-15 22:21:13 +0900

    finish studying up to 10.3.3 (final-3) - 라우터 없이 sidebar 구현 (ngSwitch문으로)

--- final/frank-simple-commerce-manager/src/app/sidebar/sidebar.component.ts
+++ final/frank-simple-commerce-manager/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+
+export declare type SidebarMenu = 'not_selected' | 'product' | 'category';
 
 @Component({
   selector: 'scm-sidebar',
@@ -6,11 +8,18 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  currentMenu: SidebarMenu;
+  @Output() changedMenu: EventEmitter<string> = new EventEmitter(); //부모 컴포넌트에 보내려고 함
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  clickedMenu(menu: SidebarMenu) {
+    this.currentMenu = menu;
+    this.changedMenu.emit(menu);
   }
 
 }
