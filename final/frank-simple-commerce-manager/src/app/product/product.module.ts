39221f582aa940d63eb3b223f1502a58a45ed1a8
ykoh <kenshin579@gmail.com>
2017-12-15 22:34:28 +0900

    finish studying up to 11.1 (final-4) - 모듈별로 분류   ㅁ. 기능, 공유, 핵심 모듈

---
+++ final/frank-simple-commerce-manager/src/app/product/product.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { ProductManagementComponent } from './product-management/product-management.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ProductManagementComponent]
+})
+export class ProductModule { }
